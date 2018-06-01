d77c063bb6e20475115080a31cd997608966851c
geekyzk <geekyzk@163.com>
2018-06-01 17:02:25 +0800

    Initial commit

---
+++ src/main/java/com/em248/boot_web_test/BootWebTestApplication.java
@@ -0,0 +1,12 @@
+package com.em248.boot_web_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWebTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/em248/boot_web_test/BootWebTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.em248.boot_web_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootWebTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
